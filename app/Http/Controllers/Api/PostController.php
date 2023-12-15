54f5a6cfb71e152d43543b5f83c5dc07d4b8c971
kimdp5959 <kimdp5959@gmail.com>
2023-12-16 00:33:33 +0900

    [Restful API] - 데이터 등록
    :등록 메소스(store) 추가

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -10,20 +10,57 @@ use App\Http\Controllers\Controller;
 
 //API 리소스 선언
 use App\Http\Resources\PostResource;
+//Facade 폼검증 선언
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
-	/**
-	 * 기본화면(리스트)
-	 *
-	 * @return void
-	 */
-	public function index()
-	{
-		//posts 전체 가져오기
-		$posts = Post::latest()->paginate(5);
+    /**
+     * 기본화면(리스트)
+     *
+     * @return void
+     */
+    public function index()
+    {
+        //posts 전체 가져오기
+        $posts = Post::latest()->paginate(5);
 
-		//API 리소스에 결과값 리턴
-		return new PostResource(true, 'List Data Posts', $posts);
-	}
+        //API 리소스에 결과값 리턴
+        return new PostResource(true, 'List Data Posts', $posts);
+    }
+
+    /**
+     * store
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        //폼 검증 유효성 규칙 선언
+        $validator = Validator::make($request->all(), [
+            'image'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title'     => 'required',
+            'content'   => 'required',
+        ]);
+
+        //폼 검증 유효성 오류시 처리
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //이미지 업로드
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        //글 작성
+        $post = Post::create([
+            'image'     => $image->hashName(),
+            'title'     => $request->title,
+            'content'   => $request->content,
+        ]);
+
+        //처리 응답값 선언
+        return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);
+    }
 }
